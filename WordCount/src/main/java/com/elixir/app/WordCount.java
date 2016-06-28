8ed072981390be816e25332bc7a8ccb66ff43513
harsh306 <harshnpathak@gmail.com>
2016-06-28 23:43:05 +0530

    License added

--- WordCount/src/main/java/com/elixir/app/WordCount.java
+++ WordCount/src/main/java/com/elixir/app/WordCount.java
@@ -1,4 +1,5 @@
 /**
+ * Copyright 2016 Harsh Pathak
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
